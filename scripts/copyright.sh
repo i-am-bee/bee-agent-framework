64eff9d090b18808d9285e917ab70350e127b561
Tomas Dvorak <toomas2d@gmail.com>
2024-08-30 17:16:27 +0200

    ci: improve copyright script
    Ref: #11

--- scripts/copyright.sh
+++ scripts/copyright.sh
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 set -e
 
 # Path to the package.json file
@@ -27,8 +28,24 @@ fi
 AUTHOR=$(jq -r '.author' "$PACKAGE_JSON_PATH")
 
 # Check if the author property is not null or empty
-if [[ ! -n "$AUTHOR" ]]; then
+if [[ -z "$AUTHOR" ]]; then
   echo "Error: author property not found in package.json"
+  exit 1
 fi
 
-nwa add -l apache -c "$AUTHOR" src dist tests
+# Check if 'nwa' command is not available and 'go' is available, then install 'nwa'
+if ! command -v nwa &> /dev/null && command -v go &> /dev/null; then
+  echo "Installing 'nwa' via 'go' (https://github.com/B1NARY-GR0UP/nwa)"
+  go install github.com/B1NARY-GR0UP/nwa@latest
+  # Ensure the GOPATH is added to the PATH environment variable
+  export PATH=$PATH:$(go env GOPATH)/bin
+fi
+
+if command -v nwa &> /dev/null; then
+  nwa add -l apache -c "$AUTHOR" src dist tests scripts
+elif command -v docker &> /dev/null; then
+  docker run -it -v "${PWD}:/src" ghcr.io/b1nary-gr0up/nwa:main add -l apache -c "$AUTHOR" src dist tests scripts
+else
+  echo "Error: 'nwa' is not available. Either install it manually or install go/docker."
+  exit 1
+fi
