a98b64fbee8250f0303b4cc47cbc7a42d7363798
Graham White <gwhite@uk.ibm.com>
2024-08-30 13:13:24 +0100

    fix: inputSchema specification, name and extraneous options

--- examples/tools/helloworld.ts
+++ examples/tools/helloworld.ts
@@ -5,16 +5,20 @@ type ToolOptions = BaseToolOptions;
 type ToolRunOptions = BaseToolRunOptions;
 
 export class HelloWorldTool extends Tool<StringToolOutput, ToolOptions, ToolRunOptions> {
-  name = "Helloworld";
+  name = "HelloWorld";
   description = "Says hello when asked for a special greeting.";
 
-  inputSchema = z.string();
+  inputSchema = z.object({
+    identifier: z
+      .string()
+      .describe("The identifier (person, object, animal, etc.) used to when saying Hello"),
+  });
 
   static {
     this.register();
   }
 
-  protected async _run(input: string, options?: BaseToolRunOptions): Promise<StringToolOutput> {
+  protected async _run(input: string): Promise<StringToolOutput> {
     return new StringToolOutput(`Hello, ${input}`);
   }
 }
