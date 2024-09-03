3ba5877aa6418c4a3f4f6693dd6d7359312dfa96
Tomas Dvorak <toomas2d@gmail.com>
2024-09-03 14:52:11 +0200

    test(tool): add arxiv e2e test

---
+++ tests/e2e/tools/arxiv.test.ts
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2024 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ArXivResponse, ArXivTool } from "@/tools/arxiv.js";
+import { beforeEach, expect } from "vitest";
+
+describe("Arxiv", () => {
+  let instance: ArXivTool;
+
+  beforeEach(() => {
+    instance = new ArXivTool();
+  });
+
+  it("Runs", async () => {
+    const response = await instance.run(
+      {
+        filters: [
+          {
+            include: [
+              {
+                value: "LLM",
+                field: "title",
+              },
+            ],
+          },
+        ],
+        maxResults: 2,
+        sort: {
+          type: "submittedDate",
+          order: "ascending",
+        },
+      },
+      {
+        signal: AbortSignal.timeout(60 * 1000),
+        retryOptions: {},
+      },
+    );
+
+    expect(response.isEmpty()).toBe(false);
+    expect(response.result.startIndex).toBe(0);
+    expect(response.result.entries.length).toBe(2);
+    for (const entry of response.result.entries) {
+      expect(entry).toMatchObject({
+        id: expect.any(String),
+        title: expect.any(String),
+        summary: expect.any(String),
+        published: expect.any(String),
+      } as ArXivResponse["entries"][0]);
+    }
+  });
+
+  it("Throws", async () => {
+    await expect(
+      instance.run({
+        id_list: ["xx"],
+      }),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(`[ToolError: Request to ArXiv API has failed!]`);
+  });
+});
