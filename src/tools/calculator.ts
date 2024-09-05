a07bf7fc7296a6d2d7e4323d12821795338db72b
MICHAEL DESMOND <mdesmond@us.ibm.com>
2024-09-04 21:16:25 -0400

    fix(tools): add disclaimer to calc tool, move limiting to constructor

--- src/tools/calculator.ts
+++ src/tools/calculator.ts
@@ -15,42 +15,56 @@
  */
 
 import {
-    BaseToolRunOptions,
+    BaseToolOptions,
     StringToolOutput,
     Tool,
     ToolInput,
   } from "@/tools/base.js";
   import { z } from "zod";
-  import { create, all } from 'mathjs'
-  
-  const math = create(all)
-  const limitedEvaluate = math.evaluate
-  math.import({
-    // most important (hardly any functional impact)
-    'import':     function () { throw new Error('Function import is disabled') },
-    'createUnit': function () { throw new Error('Function createUnit is disabled') },
-    'reviver':    function () { throw new Error('Function reviver is disabled') },
-  
-    // extra (has functional impact)
-    'evaluate':   function () { throw new Error('Function evaluate is disabled') },
-    'parse':      function () { throw new Error('Function parse is disabled') },
-    'simplify':   function () { throw new Error('Function simplify is disabled') },
-    'derivative': function () { throw new Error('Function derivative is disabled') },
-    'resolve':    function () { throw new Error('Function resolve is disabled') },
-  }, { override: true })
+  import { create, all, MathJsInstance } from 'mathjs'
 
+  /**
+   * Waring: The CalculatorTool enbales the agent (and by proxy the user) to execute arbirtary 
+   * expressions via mathjs.
+   * 
+   * Please consider the security and stability risks documented at
+   * https://mathjs.org/docs/expressions/security.html before using this tool. 
+   */
   export class CalculatorTool extends Tool<StringToolOutput> {
     name = "Calculator";
     description = `A calculator tool that performs basic arithmetic operations like addition, subtraction, multiplication, and division. 
 Only use the calculator tool if you need to perform a calculation.`;
+
     inputSchema() {
       return z.object({
         expression: z.string().describe(`The mathematical expression to evaluate (e.g., "2 + 3 * 4"). Use Mathjs basic expression syntax. Constants only.`),
       });
     }
-  
-    protected async _run({ expression }: ToolInput<this>, options?: BaseToolRunOptions) {
-      const result = limitedEvaluate(expression)
+
+    protected limitedEvaluate: any;
+
+    constructor({...options }: BaseToolOptions = {}) {
+      super(options);
+      const math = create(all)
+      this.limitedEvaluate = math.evaluate
+      // Disable use potentially vulnerable functions
+      math.import({
+        // most important (hardly any functional impact)
+        'import':     function () { throw new Error('Function import is disabled') },
+        'createUnit': function () { throw new Error('Function createUnit is disabled') },
+        'reviver':    function () { throw new Error('Function reviver is disabled') },
+      
+        // extra (has functional impact)
+        'evaluate':   function () { throw new Error('Function evaluate is disabled') },
+        'parse':      function () { throw new Error('Function parse is disabled') },
+        'simplify':   function () { throw new Error('Function simplify is disabled') },
+        'derivative': function () { throw new Error('Function derivative is disabled') },
+        'resolve':    function () { throw new Error('Function resolve is disabled') },
+      }, { override: true })
+    }
+
+    protected async _run({ expression }: ToolInput<this>) {
+      const result = this.limitedEvaluate(expression)
       return new StringToolOutput(result);
     }
   }
