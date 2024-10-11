325e1ed2f703ca77a560e24936398be52b79441b
Tomas Dvorak <toomas2d@gmail.com>
2024-10-11 13:12:10 +0200

    feat(agent): update SQL tool example
    Signed-off-by: Tomas Dvorak <toomas2d@gmail.com>

--- examples/agents/sql.ts
+++ examples/agents/sql.ts
@@ -4,12 +4,14 @@ import { GroqChatLLM } from "bee-agent-framework/adapters/groq/chat";
 import { SQLTool } from "bee-agent-framework/tools/database/sql";
 import { FrameworkError } from "bee-agent-framework/errors";
 import { UnconstrainedMemory } from "bee-agent-framework/memory/unconstrainedMemory";
+import fs from "node:fs";
+import * as path from "node:path";
+import os from "node:os";
 
 const llm = new GroqChatLLM({
-  modelId: "llama3-70b-8192",
+  modelId: "llama-3.1-70b-versatile",
   parameters: {
     temperature: 0,
-    max_tokens: 8192,
   },
 });
 
@@ -17,8 +19,19 @@ const sqlTool = new SQLTool({
   provider: "sqlite",
   connection: {
     dialect: "sqlite",
-    storage: "chinook.sqlite",
     logging: false,
+    storage: await fetch(
+      "https://github.com/lerocha/chinook-database/releases/download/v1.4.5/chinook_sqlite.sqlite",
+    ).then(async (response) => {
+      if (!response.ok) {
+        throw new Error("Failed to download Chinook database!");
+      }
+
+      const dbPath = path.join(os.tmpdir(), "bee_chinook.sqlite");
+      const data = Buffer.from(await response.arrayBuffer());
+      await fs.promises.writeFile(dbPath, data);
+      return dbPath;
+    }),
   },
 });
 
@@ -59,5 +72,4 @@ try {
   console.error(FrameworkError.ensure(error).dump());
 } finally {
   await sqlTool.destroy();
-  process.exit(0);
 }
