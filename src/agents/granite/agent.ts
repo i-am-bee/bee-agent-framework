484a424ee55bc7d539f495bb71fccb1674250d0e
Tomas Dvorak <toomas2d@gmail.com>
2024-11-08 14:53:31 +0100

    feat(agents): simplify and refactor Granite agent
    Ref: #145
    Signed-off-by: Tomas Dvorak <toomas2d@gmail.com>

--- src/agents/granite/agent.ts
+++ src/agents/granite/agent.ts
@@ -14,134 +14,51 @@
  * limitations under the License.
  */
 
-import { BaseMessage, Role } from "@/llms/primitives/message.js";
-import {
-  BeeAgentRunIteration,
-  BeeMeta,
-  BeeRunInput,
-  BeeRunOptions,
-  BeeRunOutput,
-} from "@/agents/bee/types.js";
+import { BeeRunInput, BeeRunOptions, BeeRunOutput } from "@/agents/bee/types.js";
 import { GetRunContext } from "@/context.js";
-import { BeeAgentError } from "@/agents/bee/errors.js";
-import { BeeIterationToolResult } from "@/agents/bee/parser.js";
-import { BeeAgent } from "@/agents/bee/agent.js";
-import * as R from "remeda";
+import { BeeAgent, BeeInput } from "@/agents/bee/agent.js";
 import { GraniteAgentRunner } from "./runner.js";
-import { z } from "zod";
-import { PromptTemplate } from "@/template.js";
-
-const GraniteBeeAssistantPrompt = new PromptTemplate({
-  schema: z
-    .object({
-      thought: z.array(z.string()),
-      toolName: z.array(z.string()),
-      toolInput: z.array(z.string()),
-      finalAnswer: z.array(z.string()),
-    })
-    .partial(),
-  template: `{{#thought}}Thought: {{.}}\n{{/thought}}{{#toolName}}Tool Name: {{.}}\n{{/toolName}}{{#toolInput}}Tool Input: {{.}}\n{{/toolInput}}{{#finalAnswer}}Final Answer: {{.}}{{/finalAnswer}}`,
-});
+import { GraniteBeeAssistantPrompt, GraniteBeeSystemPrompt } from "@/agents/granite/prompts.js";
+import { BaseMessage } from "@/llms/primitives/message.js";
 
 const TOOL_RESPONSE_ROLE = "tool_response";
 
 export class GraniteBeeAgent extends BeeAgent {
   protected runner: typeof GraniteAgentRunner = GraniteAgentRunner;
 
+  constructor(input: BeeInput) {
+    super({
+      ...input,
+      templates: {
+        ...input.templates,
+        system: GraniteBeeSystemPrompt,
+        assistant: GraniteBeeAssistantPrompt,
+      },
+    });
+  }
+
   protected async _run(
     input: BeeRunInput,
     options: BeeRunOptions = {},
     run: GetRunContext<typeof this>,
   ): Promise<BeeRunOutput> {
-    const iterations: BeeAgentRunIteration[] = [];
-    const maxIterations = options?.execution?.maxIterations ?? Infinity;
-    const runner = await this.runner.create(this.input, options, input.prompt);
-
-    let finalMessage: BaseMessage | undefined;
-    while (!finalMessage) {
-      const meta: BeeMeta = { iteration: iterations.length + 1 };
-      if (meta.iteration > maxIterations) {
-        throw new BeeAgentError(
-          `Agent was not able to resolve the task in ${maxIterations} iterations.`,
-          [],
-          { isFatal: true },
-        );
-      }
-
-      const emitter = run.emitter.child({ groupId: `iteration-${meta.iteration}` });
-      const iteration = await runner.llm({ emitter, signal: run.signal, meta });
-
-      if (iteration.state.tool_name || iteration.state.tool_input) {
-        const { output, success } = await runner.tool({
-          iteration: iteration.state as BeeIterationToolResult,
-          signal: run.signal,
-          emitter,
-          meta,
-        });
-
-        for (const key of ["partialUpdate", "update"] as const) {
-          await emitter.emit(key, {
-            data: {
-              ...iteration.state,
-              tool_output: output,
-            },
-            update: { key: "tool_output", value: output, parsedValue: output },
-            meta: { success, ...meta },
-          });
-        }
-
-        await runner.memory.addMany([
-          BaseMessage.of({
-            role: Role.ASSISTANT,
-            text: GraniteBeeAssistantPrompt.clone().render({
-              toolName: [iteration.state.tool_name].filter(R.isTruthy),
-              toolInput: [iteration.state.tool_input]
-                .filter(R.isTruthy)
-                .map((call) => JSON.stringify(call)),
-              thought: [iteration.state.thought].filter(R.isTruthy),
-              finalAnswer: [iteration.state.final_answer].filter(R.isTruthy),
+    const cleanup = run.emitter.on(
+      "update",
+      async ({ update, meta, memory }) => {
+        if (update.key === "tool_output") {
+          await memory.add(
+            BaseMessage.of({
+              role: TOOL_RESPONSE_ROLE,
+              text: update.value,
+              meta: { success: meta.success },
             }),
-            meta: { success },
-          }),
-          BaseMessage.of({
-            role: TOOL_RESPONSE_ROLE,
-            text: output,
-            meta: { success },
-          }),
-        ]);
-      }
-      if (iteration.state.final_answer) {
-        finalMessage = BaseMessage.of({
-          role: Role.ASSISTANT,
-          text: iteration.state.final_answer,
-          meta: {
-            createdAt: new Date(),
-          },
-        });
-        await run.emitter.emit("success", {
-          data: finalMessage,
-          iterations,
-          memory: runner.memory,
-          meta,
-        });
-        await runner.memory.add(finalMessage);
-      }
-      iterations.push(iteration);
-    }
-
-    if (input.prompt !== null) {
-      await this.input.memory.add(
-        BaseMessage.of({
-          role: Role.USER,
-          text: input.prompt,
-          meta: {
-            createdAt: run.createdAt,
-          },
-        }),
-      );
-    }
-    await this.input.memory.add(finalMessage);
-
-    return { result: finalMessage, iterations, memory: runner.memory };
+          );
+        }
+      },
+      {
+        isBlocking: true,
+      },
+    );
+    return super._run(input, options, run).finally(cleanup);
   }
 }
--- src/agents/granite/runner.ts
+++ src/agents/granite/runner.ts
@@ -17,87 +17,16 @@ import { BaseMessage, Role } from "@/llms/primitives/message.js";
 import { BeeRunOptions } from "@/agents/bee/types.js";
 import { AnyTool } from "@/tools/base.js";
 import { BeeInput } from "@/agents/bee/agent.js";
-import { z } from "zod";
 import * as R from "remeda";
 import { BeeAgentRunner } from "@/agents/bee/runner.js";
-import { PromptTemplate } from "@/template.js";
 import { BeeParserInput } from "@/agents/bee/parser.js";
 import { isEmpty } from "remeda";
-
-const graniteBeeSystemPrompt = new PromptTemplate({
-  schema: z.object({
-    instructions: z.string().default("You are a helpful assistant."),
-    tools: z.array(
-      z
-        .object({
-          name: z.string().min(1),
-          description: z.string().min(1),
-          schema: z.string().min(1),
-        })
-        .passthrough(),
-    ),
-  }),
-  template: `# Setting
-You are an AI assistant.
-When the user sends a message figure out a solution and provide a final answer.
-{{#tools.length}}
-You have access to a set of available_tools that can be used to retrieve information and perform actions.
-Pay close attention to the tool description to determine if a tool is useful in a partcular context.
-{{/tools.length}}
-{{#datetime}}
-The current date is {{datetime}}.
-{{/datetime}}
-
-# Communication structure: 
-- Line starting 'Message: ' The user's question or instruction. This is provided by the user, the assistant does not produce this.
-- Line starting 'Thought: ' The assistant's response always starts with a thought, this is free text where the assistant thinks about the user's message and describes in detail what it should do next. 
-{{#tools.length}}
-- In a 'Thought', the assistant should determine if a Tool Call is necessary to get more information or perform an action, or if the available information is sufficient to provide the Final Answer.
-- If a tool needs to be called and is available, the assistant will produce a tool call:
-- Line starting 'Tool Name: ' name of the tool that you want to use.
-- Line starting 'Tool Input: ' JSON formatted tool arguments adhering to the selected tool parameters schema i.e. {"arg1":"value1", "arg2":"value2"}. 
-- Line starting 'Thought: ', followed by free text where the assistant thinks about the all the information it has available, and what it should do next (e.g. try the same tool with a different input, try a different tool, or proceed with answering the original user question).
-{{/tools.length}}
-- Once enough information is available to provide the Final Answer, the last line in the message needs to be: 
-- Line starting 'Final Answer: ' followed by a answer to the original message.
-
-# Best practices
-- Use markdown syntax for formatting code snippets, links, JSON, tables, images, files.
-{{#tools.length}}
-- Do not attempt to use a tool that is not listed in available tools. This will cause an error.
-- Make sure that tool input is in the correct format and contains the correct arguments.
-{{/tools.length}}
-- When the message is unclear, respond with a line starting with 'Final Answer:' followed by a request for additional information needed to solve the problem.
-- When the user wants to chitchat instead, always respond politely.
-
-{{#tools.length}}
-# File Handling
-If a file is provided by the user, make sure you always use the FileRead tool immediately to read the contents of the file.
-{{/tools.length}}
-
-# Date and Time
-The current date and time is contained in the most recent user message.
-
-# Persona
-{{instructions}}
-`,
-});
 
 const AVAILABLE_TOOLS_ROLE = "available_tools";
 
 export class GraniteAgentRunner extends BeeAgentRunner {
   static async create(input: BeeInput, options: BeeRunOptions, prompt: string | null) {
-    const instance = await super.create(
-      {
-        ...input,
-        templates: {
-          ...input.templates,
-          system: graniteBeeSystemPrompt,
-        },
-      },
-      options,
-      prompt,
-    );
+    const instance = await super.create(input, options, prompt);
 
     if (!isEmpty(input.tools)) {
       const index = instance.memory.messages.findIndex((msg) => msg.role === Role.SYSTEM) + 1;
