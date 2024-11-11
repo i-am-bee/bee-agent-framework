459aeb591a47e6afd214c21c39ad7cec347f2ef3
MICHAEL DESMOND <mdesmond@us.ibm.com>
2024-11-11 16:10:29 -0500

    fix: remove unused import
    Signed-off-by: MICHAEL DESMOND <mdesmond@us.ibm.com>

--- examples/agents/bee_granite.ts
+++ examples/agents/bee_granite.ts
@@ -8,12 +8,11 @@ import { GraniteBeeAgent } from "bee-agent-framework/agents/granite/agent";
 import { WatsonXChatLLM } from "bee-agent-framework/adapters/watsonx/chat";
 import { z } from "zod";
 import { parseEnv } from "bee-agent-framework/internals/env";
-import { OpenMeteoTool } from "bee-agent-framework/tools/weather/openMeteo";
 
 // Ensure that you have added your WatsonX credentials in .env
 const llm = WatsonXChatLLM.fromPreset("ibm/granite-3-8b-instruct", {
   apiKey: parseEnv("WATSONX_API_KEY", z.string()),
-  projectId: parseEnv("WATSONX_PROJECT_ID", z.string())
+  projectId: parseEnv("WATSONX_PROJECT_ID", z.string()),
 });
 
 const agent = new GraniteBeeAgent({
