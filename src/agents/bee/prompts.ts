2c3ede319b408781e04216dd84eccefa243ec5c4
Ales Kalfas <ales.kalfas@ibm.com>
2024-12-04 19:12:07 +0100

    fixup!
    Signed-off-by: Aleš Kalfas <kalfas.ales@gmail.com>

--- src/agents/bee/prompts.ts
+++ src/agents/bee/prompts.ts
@@ -47,7 +47,7 @@ No functions are available.
 
 {{/tools.length}}
 # Communication structure
-You communicate only in instruction lines. The format is: "Instruction: expected output". You must only use these instruction lines and must not enter empty lines or anything else between instruction lines.
+You communicate only in instruction lines. The format is: "Instruction: expected output\n". You must only use these instruction lines and must not enter empty lines between them. Each instruction must start on a new line.
 {{#tools.length}}
 You must skip the instruction lines Function Name, Function Input and Function Output if no function calling is required.
 {{/tools.length}}
@@ -57,7 +57,7 @@ Message: User's message. You never use this instruction line.
 Thought: A single-line plan of how to answer the user's message. It must be immediately followed by Final Answer.
 {{/tools.length}}
 {{#tools.length}}
-Thought: A single-line step-by-step plan of how to answer the user's message. You can use the available functions defined above. This instruction line must be immediately followed by Function Name if one of the available functions defined above needs to be called, or by Final Answer. Do not provide the answer here. Always ends with new line.
+Thought: A single-line step-by-step plan of how to answer the user's message. You can use the available functions defined above. This instruction line must be immediately followed by Function Name if one of the available functions defined above needs to be called, or by Final Answer. Do not provide the answer here.
 Function Name: Name of the function. This instruction line must be immediately followed by Function Input.
 Function Input: Function parameters. Empty object is a valid parameter.
 Function Output: Output of the function in JSON format.
