972681ffcf6cfdfa8d176649adc3197a97bcd118
Graham White <grahamwhiteuk@users.noreply.github.com>
2024-12-13 11:50:09 +0000

    feat(adapters): add presets for Granite 3.1 in WatsonX and IBM vLLM (#250)
    Signed-off-by: Graham White <gwhite@uk.ibm.com>

--- src/adapters/watsonx/chatPreset.ts
+++ src/adapters/watsonx/chatPreset.ts
@@ -61,6 +61,25 @@ export const WatsonXChatLLMPreset = {
   "ibm/granite-3-2b-instruct"() {
     return WatsonXChatLLMPreset["ibm/granite-3-8b-instruct"]();
   },
+  "ibm/granite-3-1-8b-instruct": (): WatsonXChatLLMPreset => {
+    const { template, parameters, messagesToPrompt } = LLMChatTemplates.get("granite3Instruct");
+    return {
+      base: {
+        parameters: {
+          decoding_method: "greedy",
+          max_new_tokens: 512,
+          include_stop_sequence: false,
+          stop_sequences: [...parameters.stop_sequence],
+        },
+      },
+      chat: {
+        messagesToPrompt: messagesToPrompt(template),
+      },
+    };
+  },
+  "ibm/granite-3-1-2b-instruct"() {
+    return WatsonXChatLLMPreset["ibm/granite-3-8b-instruct"]();
+  },
   "meta-llama/llama-3-1-70b-instruct": (): WatsonXChatLLMPreset => {
     const { template, messagesToPrompt, parameters } = LLMChatTemplates.get("llama3.1");
 
