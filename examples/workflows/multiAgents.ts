72bfab667fa11096f5e044cb2ad01d561bb324ee
Tomas Dvorak <toomas2d@gmail.com>
2025-01-22 19:13:32 +0100

    docs(workflows): update examples
    Signed-off-by: Tomas Dvorak <toomas2d@gmail.com>

--- examples/workflows/multiAgents.ts
+++ examples/workflows/multiAgents.ts
@@ -5,27 +5,32 @@ import { OpenMeteoTool } from "bee-agent-framework/tools/weather/openMeteo";
 import { WikipediaTool } from "bee-agent-framework/tools/search/wikipedia";
 import { AgentWorkflow } from "bee-agent-framework/experimental/workflows/agent";
 import { BaseMessage, Role } from "bee-agent-framework/llms/primitives/message";
-import { GroqChatLLM } from "bee-agent-framework/adapters/groq/chat";
+import { WatsonXChatLLM } from "bee-agent-framework/adapters/watsonx/chat";
 
 const workflow = new AgentWorkflow();
+const llm = WatsonXChatLLM.fromPreset("meta-llama/llama-3-3-70b-instruct", {
+  apiKey: process.env.WATSONX_API_KEY,
+  projectId: process.env.WATSONX_PROJECT_ID,
+});
+
 workflow.addAgent({
   name: "WeatherForecaster",
   instructions: "You are a weather assistant. Respond only if you can provide a useful answer.",
   tools: [new OpenMeteoTool()],
-  llm: new GroqChatLLM(),
+  llm,
   execution: { maxIterations: 3 },
 });
 workflow.addAgent({
   name: "Researcher",
   instructions: "You are a researcher assistant. Respond only if you can provide a useful answer.",
   tools: [new WikipediaTool()],
-  llm: new GroqChatLLM(),
+  llm,
 });
 workflow.addAgent({
   name: "Solver",
   instructions:
     "Your task is to provide the most useful final answer based on the assistants' responses which all are relevant. Ignore those where assistant do not know.",
-  llm: new GroqChatLLM(),
+  llm,
 });
 
 const reader = createConsoleReader();
@@ -45,6 +50,9 @@ for await (const { prompt } of reader) {
       reader.write(`-> ${data.step}`, data.response?.update?.finalAnswer ?? "-");
     });
   });
-  await memory.addMany(result.newMessages);
+
+  // await memory.addMany(result.newMessages); // save intermediate steps + final answer
+  await memory.addMany(result.newMessages.slice(-1)); // save only the final answer
+
   reader.write(`Agent 🤖`, result.finalAnswer);
 }
