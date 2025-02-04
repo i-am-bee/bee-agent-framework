a0096d3b5698faa1d71234de9aff24a851743e20
Petr Bulánek <bulanek.petr@gmail.com>
2025-02-04 17:24:52 +0100

    fixup! fixup! feat(ui): add basic setup for beeai-ui app

--- apps/beeai-ui/src/main.tsx
+++ apps/beeai-ui/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './AppTemp';
+import { App } from './App';
 import './styles/style.scss';
 
 createRoot(document.getElementById('root')!).render(
