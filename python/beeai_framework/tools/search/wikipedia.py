000ccee2497061cb31f15416dafaa90b30dfe153
AngeloDanducci <angelo.danducci.ii@ibm.com>
2025-02-20 12:09:51 -0500

    fix: fix linting and formatting
    Signed-off-by: AngeloDanducci <angelo.danducci.ii@ibm.com>

--- python/beeai_framework/tools/search/wikipedia.py
+++ python/beeai_framework/tools/search/wikipedia.py
@@ -16,19 +16,20 @@
 from typing import Any
 
 import wikipediaapi
-
 from pydantic import BaseModel, Field
 
 from beeai_framework.tools import ToolError
 from beeai_framework.tools.search import SearchToolOutput, SearchToolResult
 from beeai_framework.tools.tool import Tool
-from beeai_framework.utils import BeeLogger
+
 
 class WikipediaSearchToolInput(BaseModel):
     query: str = Field(description="Search query, name of the Wikipedia page.")
 
+
 class WikipediaSearchToolResult(SearchToolResult):
     pass
+
 
 class WikipediaSearchToolOutput(SearchToolOutput):
     pass
@@ -36,9 +37,13 @@ class WikipediaSearchToolOutput(SearchToolOutput):
 
 class WikipediaSearchTool(Tool[WikipediaSearchToolInput]):
     name = "Wikipedia"
-    description = "Search factual and historical information, including biography, history, politics, geography, society, culture, science, technology, people, animal species, mathematics, and other subjects."
+    description = "Search factual and historical information, including biography, \
+        history, politics, geography, society, culture, science, technology, people, \
+        animal species, mathematics, and other subjects."
     input_schema = WikipediaSearchToolInput
-    client = wikipediaapi.Wikipedia(user_agent="beeai-framework https://github.com/i-am-bee/beeai-framework", language='en')
+    client = wikipediaapi.Wikipedia(
+        user_agent="beeai-framework https://github.com/i-am-bee/beeai-framework", language="en"
+    )
 
     def __init__(self, full_text: bool = False) -> None:
         super().__init__()
@@ -47,8 +52,8 @@ class WikipediaSearchTool(Tool[WikipediaSearchToolInput]):
     def _run(self, input: WikipediaSearchToolInput, _: Any | None = None) -> WikipediaSearchToolOutput:
         try:
             page_py = self.client.page(input.query)
-            if(page_py.exists()):
-                description_output = page_py.text if self.full_text else page_py.summary 
+            if page_py.exists():
+                description_output = page_py.text if self.full_text else page_py.summary
                 search_results: list[WikipediaSearchToolResult] = [
                     WikipediaSearchToolResult(
                         title=input.query or "", description=description_output or "", url=page_py.fullurl or ""
@@ -56,6 +61,6 @@ class WikipediaSearchTool(Tool[WikipediaSearchToolInput]):
                 ]
                 return WikipediaSearchToolOutput(search_results)
             else:
-                raise Exception("No Wikipedia page matched the search term: %s." % (input.query))
+                raise Exception(f"No Wikipedia page matched the search term: {input.query}.")
         except Exception as e:
             raise ToolError("Error performing Wikipedia search: ") from e
--- python/examples/tools/wikipedia.py
+++ python/examples/tools/wikipedia.py
@@ -6,11 +6,12 @@ from beeai_framework.backend.chat import ChatModel
 from beeai_framework.memory import UnconstrainedMemory
 from beeai_framework.tools.search.wikipedia import WikipediaSearchTool
 
+
 async def main() -> None:
     chat_model = await ChatModel.from_name("ollama:granite3.1-dense:8b")
     agent = BeeAgent(BeeInput(llm=chat_model, tools=[WikipediaSearchTool()], memory=UnconstrainedMemory()))
 
-    result = await agent.run(BeeRunInput(prompt = "What is the Wikipedia information for Bees"))
+    result = await agent.run(BeeRunInput(prompt="What is the Wikipedia information for Bees"))
 
     print(result.result.text)
 
